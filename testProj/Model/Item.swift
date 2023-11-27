972c7368882b3d861b17de1857226905d088af28
Arthur Sh <arthur.shemshur@gmail.com>
2023-11-27 20:27:13 +0200

    created new model and viewmodel

---
+++ testProj/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  testProj
+//
+//  Created by Arthur Sh on 27.11.2023.
+//
+
+import Foundation
+
+struct Item {
+    var name: String
+    var price: Int
+    var isAvailable: Bool
+    
+    
+}
---
+++ testProj/ViewModel/ItemsLogic.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsLogic.swift
+//  testProj
+//
+//  Created by Arthur Sh on 27.11.2023.
+//
+
+import Foundation
+
+
+struct ContentViewModel {
+    var items = [Item]()
+}
