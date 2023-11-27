adf3640e4f397ce4dfd061b4926efa9ebf823359
Arthur Sh <arthur.shemshur@gmail.com>
2023-11-27 18:54:01 +0200

    Initial Commit

---
+++ testProj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testProj
+//
+//  Created by Arthur Sh on 27.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testProj/testProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjApp.swift
+//  testProj
+//
+//  Created by Arthur Sh on 27.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct testProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
